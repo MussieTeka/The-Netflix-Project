3e158c09012b9d1127e090c9a9ef6aa5afb8c0f6
Mussie Kahsay <mussiekahasay12@gmail.com>
2023-07-15 17:18:06 +0300

    Add src/script.js file.

--- src/script.js
+++ src/script.js
@@ -1,1 +1,22 @@
 import './style.css';
+
+const accordian = document.getElementsByClassName('FAQ__title');
+
+for (let i = 0; i < accordian.length; i += 1) {
+  accordian[i].addEventListener('click', function toggleAccordian() {
+    if (this.childNodes[1].classList.contains('fa-plus')) {
+      this.childNodes[1].classList.remove('fa-plus');
+      this.childNodes[1].classList.add('fa-times');
+    } else {
+      this.childNodes[1].classList.remove('fa-times');
+      this.childNodes[1].classList.add('fa-plus');
+    }
+
+    const content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = `${content.scrollHeight}px`;
+    }
+  });
+}
